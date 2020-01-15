5284ac5861761abd1ddb16f44233742502918d11
alicezeus <59837479+alicezeus@users.noreply.github.com>
2020-01-15 20:34:04 +0000

    game

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,34 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+// 1. find the valuable things.
+
+const secretnumber = 6;
+
+const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+let currentSquare = 1;
+squares.forEach(sq => {
+
+    if (currentSquare === secretnumber) {
+        sq.dataset.secret = 'true';
+    }
+    currentSquare++;
+    sq.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    const that = this as HTMLDivElement;
+    const isWinner = that.dataset.secret === 'true';
+    if (isWinner) {
+        that.classList.add('winner');
+    } else {
+        that.classList.add('loser');
+    }
+    that.removeEventListener('click', handleClick);
+}
+
+// Once you win, all the other guesses should be marked as losers.
+// Maybe make a play again buttun that resets everything.
+// if you win, display a message telling that how awesome they are.
+// if they lost (YOU PICKED EVERY ONE BUT THE WINNER) they should be told they lose.
